94ab165fe20e4d34306d0a148db21362aaf93549
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-19 10:22:04 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+
+//1) is declared as equal to Chelsea
+// 2) is defined using let
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+
+//3) is declared as equal to Susan Smith
+//4) is defined using let
+
+let companyCeo = 'Susan Smith';
